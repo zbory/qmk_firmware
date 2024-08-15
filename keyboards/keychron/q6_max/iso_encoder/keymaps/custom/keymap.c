3772c7871d04927c7fde98cc19b3fe3964ebe8a6
zbory <zborayb@gmail.com>
2024-08-15 21:43:12 +0200

    Make rotary change RGB hue

--- keyboards/keychron/q6_max/iso_encoder/keymaps/custom/keymap.c
+++ keyboards/keychron/q6_max/iso_encoder/keymaps/custom/keymap.c
@@ -70,9 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [MAC_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [MAC_FN]   = {ENCODER_CCW_CW(RGB_HUI, RGB_HUD)},
     [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [WIN_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [WIN_FN]   = {ENCODER_CCW_CW(RGB_HUI, RGB_HUD)},
 };
 #endif // ENCODER_MAP_ENABLE
 
