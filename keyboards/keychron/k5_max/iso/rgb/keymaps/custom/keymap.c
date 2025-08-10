ed0708b4c7a3243c20b7e9c10d62a1de0191c68d
zbory <zborayb@gmail.com>
2025-08-10 13:03:46 +0200

    Basic build and git commands

---
+++ keyboards/keychron/k5_max/iso/rgb/keymaps/custom/keymap.c
@@ -0,0 +1,146 @@
+/* Copyright 2024 ~ 2025 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "keychron_common.h"
+
+enum macro_keycodes {
+    BUILD_ITF = NEW_SAFE_RANGE,
+    BUILD_OCA,
+    JOKER_CMD,
+    GIT_STASH,
+    GIT_POP,
+    GIT_STATUS,
+    GIT_FORCE_PUSH
+};
+
+enum layers {
+    MAC_BASE,
+    MAC_FN,
+    WIN_BASE,
+    WIN_FN,
+};
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [MAC_BASE] = LAYOUT_109_iso(
+        KC_ESC,             KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU, KC_SNAP,  KC_SIRI,  RGB_MOD, KC_F13,  KC_F14,  KC_F15,  KC_F16,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,           KC_DEL,   KC_END,   KC_PGDN, KC_P7,   KC_P8,   KC_P9,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,                               KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,           KC_UP,             KC_P1,   KC_P2,   KC_P3,
+        KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD, KC_ROPTN,MO(MAC_FN),KC_RCTL, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_P0,            KC_PDOT, KC_PENT),
+
+    [MAC_FN] = LAYOUT_109_iso(
+        _______,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  _______,  _______,  RGB_TOG, _______, _______, _______, _______,
+        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  _______, _______, _______, _______, _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           _______,  _______,  _______, _______, _______, _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                              _______, _______, _______, _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,           _______,           _______, _______, _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______, _______,  _______,  _______, _______,          _______, _______),
+
+    [WIN_BASE] = LAYOUT_109_iso(
+        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_PSCR,  KC_CTANA, RGB_MOD, BUILD_ITF, BUILD_OCA, _______, JOKER_CMD,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP, KC_NUM,    KC_PSLS,   KC_PAST, KC_PMNS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,           KC_DEL,   KC_END,   KC_PGDN, KC_P7,     KC_P8,     KC_P9,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,                               KC_P4,     KC_P5,     KC_P6,   KC_PPLS,
+        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,           KC_UP,             KC_P1,     KC_P2,     KC_P3,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN, MO(WIN_FN),KC_RCTL, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_P0,                KC_PDOT, KC_PENT),
+
+    [WIN_FN] = LAYOUT_109_iso(
+        _______,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU, _______,   _______,        RGB_TOG, _______, _______, _______, _______,
+        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,   _______,        _______, _______, _______, _______, _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           _______,   _______,        _______, _______, _______, _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                                     _______, _______, _______, _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,            GIT_FORCE_PUSH,          _______, _______, _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______, GIT_STASH, GIT_STATUS,     GIT_POP, _______,          _______, _______),
+};
+
+void VSCode_switch_to_terminal(void) {
+    register_code(KC_LCTL);      // Hold Left Ctrl
+    tap_code(KC_DOWN);           // Tap Down Arrow
+    unregister_code(KC_LCTL);    // Release Left Ctrl
+}
+
+void VSCode_switch_to_editor(void) {
+    register_code(KC_LCTL);      // Hold Left Ctrl
+    tap_code(KC_UP);             // Tap Up Arrow
+    unregister_code(KC_LCTL);    // Release Left Ctrl
+}
+
+bool process_record_custom_macros(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case BUILD_ITF:
+        if (record->event.pressed) {
+                VSCode_switch_to_terminal();
+                SEND_STRING("build" SS_TAP(X_ENTER));
+        }
+        return false;
+    case BUILD_OCA:
+        if (record->event.pressed) {
+                VSCode_switch_to_terminal();
+                SEND_STRING("oca" SS_TAP(X_ENTER));
+        }
+        return false;
+    case JOKER_CMD:
+        if (record->event.pressed) {
+                VSCode_switch_to_terminal();
+                SEND_STRING("." SS_LSFT(SS_TAP(X_6)) "joker.sh" SS_TAP(X_ENTER));
+        }
+        return false;
+    case KC_PSCR:
+        if (record->event.pressed) {
+                SEND_STRING(SS_LSFT(SS_LWIN("s")));
+        }
+        return false;
+    case GIT_STASH:
+        if (record->event.pressed) {
+                VSCode_switch_to_terminal();
+                SEND_STRING("git stash " SS_TAP(X_SLASH) "u" SS_TAP(X_ENTER));
+        }
+        return false;
+    case GIT_POP:
+        if (record->event.pressed) {
+                VSCode_switch_to_terminal();
+                SEND_STRING("git stash pop" SS_TAP(X_ENTER));
+        }
+        return false;
+    case GIT_STATUS:
+        if (record->event.pressed) {
+                VSCode_switch_to_terminal();
+                SEND_STRING("git fetch " SS_ALGR(SS_TAP(X_C)) SS_ALGR(SS_TAP(X_C)) " git status" SS_TAP(X_ENTER));
+        }
+        return false;
+    case GIT_FORCE_PUSH:
+        if (record->event.pressed) {
+                VSCode_switch_to_terminal();
+                SEND_STRING("git fetch " SS_ALGR(SS_TAP(X_C)) SS_ALGR(SS_TAP(X_C)) " git push " SS_TAP(X_SLASH) SS_TAP(X_SLASH) "force-with-lease" SS_TAP(X_ENTER));
+        }
+        return false;
+    };
+    return true;
+}
+
+// clang-format on
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_keychron_common(keycode, record)) {
+        return false;
+    }
+    if (!process_record_custom_macros(keycode, record)) {
+        return false;
+    }
+    return true;
+}
